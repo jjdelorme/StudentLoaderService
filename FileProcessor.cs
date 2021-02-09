3924fa0e4dbd6ee6d9fa2b1710542180489343a8
jasondel <jasondel@WINDOWS2016-SAM>
2021-02-09 22:30:35 +0000

    Add project files.

---
+++ FileProcessor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Configuration;
+
+namespace Cymbal
+{
+    public class FileProcessor
+    {
+        private string _connectionString;
+        
+        public FileProcessor()
+        {
+            _connectionString = GetConnectionString();
+        }
+
+        public void ProcessCsvFile(string file)
+        {
+            FileStream fs = File.Open(file, FileMode.Open, FileAccess.Read, FileShare.None);
+            using (StreamReader sr = new StreamReader(fs, Encoding.UTF8))
+            {
+                string line = sr.ReadLine();
+                ProcessLine(line);
+            }
+        }
+
+        private void ProcessLine(string line)
+        {
+            string[] values = line.Split(',');
+            string name = values[0];
+            string color = values[1];
+            int age = int.Parse(values[2]);
+
+            if (age < 18)
+                throw new ApplicationException("All participants must be 18 or older.");
+
+            Save(name, color, age);
+        }
+
+        private void Save(string name, string color, int age)
+        {
+            //
+        }
+
+        private string GetConnectionString()
+        {
+            return ConfigurationManager.AppSettings["DbConnectionString"];
+        }
+    }
+}
---
+++ FileProcessorService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace Cymbal
+{
+    public class FileProcessorService : ServiceBase
+    {
+        private FileSystemWatcher _fileWatcher;
+        private FileProcessor _processor;
+
+        public FileProcessorService()
+        {
+            this.ServiceName = "Cymbal.FileProcessorService";
+            this.AutoLog = true;
+
+            _processor = new FileProcessor();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                string watchPath = GetWatchPath();
+
+                _fileWatcher = new FileSystemWatcher(watchPath, "*.csv");
+                _fileWatcher.Created += OnFileCreated;
+                _fileWatcher.EnableRaisingEvents = true;
+
+                EventLog.WriteEntry("OnStart:: Watching directory " + watchPath, 
+                    EventLogEntryType.Information);
+            }
+            catch (Exception e)
+            {
+                EventLog.WriteEntry("OnStart:: Error starting service " + e.Message,
+                    EventLogEntryType.Error);
+
+                this.Stop();
+            }
+        }
+
+        private void OnFileCreated(object sender, FileSystemEventArgs e)
+        {
+            string filename = e.FullPath;
+            try
+            {
+                _processor.ProcessCsvFile(filename);
+                
+                EventLog.WriteEntry("OnFileCreated:: Processed " + filename,
+                    EventLogEntryType.Information);
+            }
+            catch (Exception ex)
+            {
+                EventLog.WriteEntry("OnFileCreated:: Error processing " + filename + " " + 
+                    ex.Message, EventLogEntryType.Error);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            if (_fileWatcher != null)
+            {
+                _fileWatcher.EnableRaisingEvents = false;
+                _fileWatcher.Dispose();
+            }
+        }
+
+        private string GetWatchPath()
+        {
+            string watchPath = ConfigurationManager.AppSettings["WatchPath"];
+            
+            if (string.IsNullOrEmpty(watchPath))
+            {
+                throw new ApplicationException(
+                    "AppSettings missing or does not contain a WatchPath value.");
+            }
+
+            if (!File.Exists(watchPath))
+            {
+                throw new ApplicationException(
+                    "Configured WatchPath directory does not exist: " + watchPath);
+            }
+            
+            return watchPath;
+        }
+    }
+}
